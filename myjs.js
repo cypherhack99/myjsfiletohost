184dfb970d01dede88c2a8a90c1cdbbd786eb3d1
cypherhack99 <58860117+cypherhack99@users.noreply.github.com>
2022-03-03 18:41:58 +0530

    Create myjs.js

---
+++ myjs.js
@@ -0,0 +1,7 @@
+<!--[ Script to activate ToC ]-->
+<script>document.addEventListener('DOMContentLoaded', () =>
+  new TableOfContents({
+      from: document.querySelector('#postBody'),
+      to: document.querySelector('#toContent')
+  }).generateToc()
+);</script>
