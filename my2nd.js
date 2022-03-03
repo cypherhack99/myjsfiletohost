a5674b2c1ce86b064f17c42ea82ac5c1372555b8
cypherhack99 <58860117+cypherhack99@users.noreply.github.com>
2022-03-03 19:53:14 +0530

    Create my2nd.js

---
+++ my2nd.js
@@ -0,0 +1,8 @@
+<!--[ Script to activate ToC ]-->
+
+  document.addEventListener('DOMContentLoaded', () =>
+  new TableOfContents({
+      from: document.querySelector('#postBody'),
+      to: document.querySelector('#toContent')
+  }).generateToc()
+);
